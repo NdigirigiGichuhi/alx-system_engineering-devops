3e70869b78f2f4b493c08f7ef79122a9830fdf77
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-01-22 05:46:42 +0300

    fixed werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,12 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
+}
+
+package { 'python3-pip':
+  ensure => 'present',
+}
