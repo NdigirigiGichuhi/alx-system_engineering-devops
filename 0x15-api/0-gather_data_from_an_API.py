6fa9ce065bfd3abf3fd554b2af38c821f9a02dcb
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-03-30 06:05:05 +0300

    question 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Module returns info about a given employee ID"""
+
+import requests
+import sys
+import json
+
+url = "https://jsonplaceholder.typicode.com"
+
+employee_id = sys.argv[1]
+
+user_url = f'{url}/users?id={employee_id}'
+
+response = requests.get(user_url)
+data = response.text
+data = json.loads(data)
+name = data[0]['name']
+
+todos_url = f'{url}/todos?userId={employee_id}'
+todos_response = requests.get(todos_url)
+tasks = todos_response.text
+tasks = json.loads(tasks)
+
+complete = 0
+all_todos = len(tasks)
+completed_tasks = []
+
+for task in tasks:
+    if task.get('completed'):
+        complete += 1
+        completed_tasks.append(task)
+
+print(f'Employee {name} is done with tasks {complete}/{all_todos}:')
+
+for task in tasks:
+    if task.get('completed'):
+        print(task['title'])
