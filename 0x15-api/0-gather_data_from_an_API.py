c0fe04b30e49e4cb3d1fbfea1d7c32d242af9e90
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-03-30 06:12:16 +0300

    question 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,30 +7,31 @@ import json
 
 url = "https://jsonplaceholder.typicode.com"
 
-employee_id = sys.argv[1]
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
 
-user_url = f'{url}/users?id={employee_id}'
+    user_url = f'{url}/users?id={employee_id}'
 
-response = requests.get(user_url)
-data = response.text
-data = json.loads(data)
-name = data[0]['name']
+    response = requests.get(user_url)
+    data = response.text
+    data = json.loads(data)
+    name = data[0]['name']
 
-todos_url = f'{url}/todos?userId={employee_id}'
-todos_response = requests.get(todos_url)
-tasks = todos_response.text
-tasks = json.loads(tasks)
+    todos_url = f'{url}/todos?userId={employee_id}'
+    todos_response = requests.get(todos_url)
+    tasks = todos_response.text
+    tasks = json.loads(tasks)
 
-complete = 0
-all_todos = len(tasks)
-completed_tasks = []
+    complete = 0
+    all_todos = len(tasks)
+    completed_tasks = []
 
-for task in tasks:
-    if task.get('completed'):
-        complete += 1
-        completed_tasks.append(task)
+    for task in tasks:
+        if task.get('completed'):
+            complete += 1
+            completed_tasks.append(task)
 
-print(f'Employee {name} is done with tasks {complete}/{all_todos}:')
+    print(f'Employee {name} is done with tasks {complete}/{all_todos}:')
 
-for task in completed_tasks:
-        print(f'\t {task.get("title")}')
+    for task in completed_tasks:
+            print(f'\t {task.get("title")}')
