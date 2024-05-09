7cc493421665206d09ffeefed119df83bf8b296b
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-05-09 23:07:24 +0300

    question 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,9 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """Queries the Reddit API and returns the number of subscribers (not
+    active users, total subscribers) for a given subreddit"""
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'MyBot/0.0.1'}
     response = requests.get(url, headers=headers, allow_redirects=False)
