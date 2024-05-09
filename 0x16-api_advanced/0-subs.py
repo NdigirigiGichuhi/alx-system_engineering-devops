53deb8093b358f678d25dfe418b48c34b549e3b9
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-05-09 23:05:23 +0300

    question 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,5 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
+        return response.json()['data']['subscribers']
+    return 0
