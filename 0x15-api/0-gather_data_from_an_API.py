e21d8ed7c56a8d5e741ac54e5b1b45e54743cfa7
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-05-11 05:32:17 +0300

    0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,28 +6,29 @@ import requests
 import sys
 
 url = 'https://jsonplaceholder.typicode.com'
-employee_id = sys.argv[1]
-response = requests.get(f'{url}/users?id={employee_id}')
+if len(sys.argv) == 2:
+    employee_id = sys.argv[1]
+    response = requests.get(f'{url}/users?id={employee_id}')
 
-data = response.json()
+    data = response.json()
 
-for item in data:
-    name = item.get('name')
+    for item in data:
+        name = item.get('name')
 
 
-tasks = requests.get(f'{url}/users/{employee_id}/todos')
-todos = tasks.json()
-my_list = []
-completed = 0
-total = 0
+    tasks = requests.get(f'{url}/users/{employee_id}/todos')
+    todos = tasks.json()
+    my_list = []
+    completed = 0
+    total = 0
 
-for item in todos:
-    total += 1
-    if item['completed']:
-        completed += 1
+    for item in todos:
+        total += 1
+        if item['completed']:
+            completed += 1
 
-print(f'Employee {name} is done with tasks {completed}/{total}:')
+    print(f'Employee {name} is done with tasks {completed}/{total}:')
 
-for item in todos:
-    if item['completed']:
-        print('\t', item['title'])
+    for item in todos:
+        if item['completed']:
+            print('\t', item['title'])
