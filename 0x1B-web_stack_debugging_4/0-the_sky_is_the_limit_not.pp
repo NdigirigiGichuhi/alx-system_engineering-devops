5058f534147766d037b1ea6f26e16a06df53b076
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-05-16 23:09:36 +0300

    0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,13 @@
 # Increases the  traffic server can handle.
 
+# Increase the ULIMIT
 exec { 'fix-config-nginx':
   onlyif  => 'test -e /etc/default/nginx',
   command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx',
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
 }
 
+#restart nginx server
 exec { 'nginx-restart':
   command => 'nginx restart',
   path    => '/etc/init.d/'
