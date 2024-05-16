c70133337b5c00c168a3b51783f3955cdbf3d6cd
NdigirigiGichuhi <ndigirigigichuhi@gmail.com>
2024-05-16 22:59:46 +0300

    0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # Increases the  traffic server can handle.
 
-exec { 'fix-nginx':
+exec { 'fix--for-nginx':
   command => 'sed -i "s/15/4096/" /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 } ->
